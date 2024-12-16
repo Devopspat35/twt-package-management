79329f7e5fa4c5d2da2d954779df49e44e881932
Devopspat35 <patemf2000.com>
2024-12-16 07:49:44 -0500

    twt-package-management- commit

---
+++ push.sh
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "twt-package-management- commit"
+git branch -M master
+git remote add org  https://github.com/Devopspat35/twt-package-management.git
+git push -u org  master
